82c91d2e4bcb151905e4ce83ef4533f2889a3bee
Diljith A K <diljithak07@gmail.com>
2024-07-16 07:52:45 +0530

    Email automation with recipient name

--- automation_103/main.py
+++ automation_103/main.py
@@ -2,17 +2,19 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formataddr
+from jinja2 import Template
 
 # Email credentials and server configuration
 email = 'testsmtp@gmail.com'  # Your SMTP email address
 password = 'your-smtp-password'  # Your SMTP password
 
 # List of recipient email addresses
-email_list = [
-    'receivermail1@gmail.com',
-    'receivermail2@gmail.com',
-    'receivermail3@gmail.com',
-    'receivermail4@gmail.com'
+recipients = [
+    {'email': 'alan1@gmail.com', 'name': 'Alan'},
+    {'email': 'nanesh2@gmail.com', 'name': 'Nanesh'},
+    {'email': 'sanal3@gmail.com', 'name': 'Sanal'},
+    {'email': 'adil4@gmail.com', 'name': 'Adil'},
+    {'email': 'savad5@gmail.com', 'name': 'Savad'}
 ]
 
 # Path to email template
@@ -33,20 +35,28 @@ if __name__ == '__main__':
         smtp.starttls()
         smtp.login(email, password)
 
-        # Create the email
-        msg = MIMEMultipart('alternative')
-        msg['From'] = formataddr(('Your Name', email))
-        msg['Subject'] = "My Email Subject"
-
         # Load email content
-        html = load_email_template(EMAIL_TEMPLATE)
-        if not html:
-            raise Exception('Failed to load email content')
+        template_str = load_email_template(EMAIL_TEMPLATE)
+        if not template_str:
+            raise Exception('Failed to load email content.')
+        template = Template(template_str)
 
-        msg.attach(MIMEText(html, 'html'))
+        for recipient in recipients:
+            name = recipient['name']
+            recipient_email = recipient['email']
 
-        # Send the email
-        smtp.sendmail(email, email_list, msg.as_string())
+            # Create the email
+            msg = MIMEMultipart('alternative')
+            msg['From'] = formataddr(('Your Name', email))
+            msg['To'] = recipient_email
+            msg['Subject'] = "My Email Subject"
+
+            # Render HTML content with recipient full name
+            html_content = template.render(name=name)
+            msg.attach(MIMEText(html_content, 'html'))
+
+            # Send the email
+            smtp.sendmail(email, recipient_email, msg.as_string())
         
         # Quit the SMTP server
         smtp.quit()
